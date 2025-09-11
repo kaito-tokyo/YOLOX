590bae7b57a279f52cb729d29e9e869a86151e66
Kaito Udagawa <umireon@gmail.com>
2025-09-12 01:21:43 +0900

    Migrate packaging to PEP 621 pyproject.toml
    Moved project metadata and dependencies from setup.py and requirements.txt to pyproject.toml using PEP 621 standards. Removed requirements.txt and simplified setup.py to rely on pyproject.toml for configuration.

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # Copyright (c) Megvii, Inc. and its affiliates. All Rights Reserved
 
-import re
 import setuptools
 import sys
 
@@ -20,28 +19,6 @@ def get_package_dir():
         "yolox.exp.default": "exps/default",
     }
     return pkg_dir
-
-
-def get_install_requirements():
-    with open("requirements.txt", "r", encoding="utf-8") as f:
-        reqs = [x.strip() for x in f.read().splitlines()]
-    reqs = [x for x in reqs if not x.startswith("#")]
-    return reqs
-
-
-def get_yolox_version():
-    with open("yolox/__init__.py", "r") as f:
-        version = re.search(
-            r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-            f.read(), re.MULTILINE
-        ).group(1)
-    return version
-
-
-def get_long_description():
-    with open("README.md", "r", encoding="utf-8") as f:
-        long_description = f.read()
-    return long_description
 
 
 def get_ext_modules():
@@ -69,27 +46,8 @@ def get_cmd_class():
 
 
 setuptools.setup(
-    name="yolox",
-    version=get_yolox_version(),
-    author="megvii basedet team",
-    url="https://github.com/Megvii-BaseDetection/YOLOX",
     package_dir=get_package_dir(),
     packages=setuptools.find_packages(exclude=("tests", "tools")) + list(get_package_dir().keys()),
-    python_requires=">=3.6",
-    install_requires=get_install_requirements(),
-    setup_requires=["wheel"],  # avoid building error when pip is not updated
-    long_description=get_long_description(),
-    long_description_content_type="text/markdown",
-    include_package_data=True,  # include files in MANIFEST.in
     ext_modules=get_ext_modules(),
     cmdclass=get_cmd_class(),
-    classifiers=[
-        "Programming Language :: Python :: 3", "Operating System :: OS Independent",
-        "License :: OSI Approved :: Apache Software License",
-    ],
-    project_urls={
-        "Documentation": "https://yolox.readthedocs.io",
-        "Source": "https://github.com/Megvii-BaseDetection/YOLOX",
-        "Tracker": "https://github.com/Megvii-BaseDetection/YOLOX/issues",
-    },
 )
