90e29fa9f67c9ff9e5f5898c88d04a42f2c7bc9f
Kaito Udagawa <umireon@gmail.com>
2025-09-12 07:03:18 +0900

    Update export_onnx.py

--- tools/export_onnx.py
+++ tools/export_onnx.py
@@ -32,7 +32,6 @@ def make_parser():
     parser.add_argument(
         "--dynamic", action="store_true", help="whether the input shape should be dynamic or not"
     )
-    # parser.add_argument("--no-onnxsim", action="store_true", help="use onnxsim or not")
     parser.add_argument(
         "-f",
         "--exp_file",
@@ -99,17 +98,6 @@ def main():
         opset_version=args.opset,
     )
     logger.info("generated onnx model named {}".format(args.output_name))
-
-    # if not args.no_onnxsim:
-    #     import onnx
-    #     from onnxsim import simplify
-
-    #     # use onnx-simplifier to reduce reduent model.
-    #     onnx_model = onnx.load(args.output_name)
-    #     model_simp, check = simplify(onnx_model)
-    #     assert check, "Simplified ONNX model could not be validated"
-    #     onnx.save(model_simp, args.output_name)
-    #     logger.info("generated simplified onnx model named {}".format(args.output_name))
 
 
 if __name__ == "__main__":
