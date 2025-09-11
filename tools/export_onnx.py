f5a127a1e06143249115cf462db1ba28fb2ffef5
Kaito Udagawa <umireon@gmail.com>
2025-09-12 03:09:43 +0900

    Update export_onnx.py

--- tools/export_onnx.py
+++ tools/export_onnx.py
@@ -32,7 +32,7 @@ def make_parser():
     parser.add_argument(
         "--dynamic", action="store_true", help="whether the input shape should be dynamic or not"
     )
-    parser.add_argument("--no-onnxsim", action="store_true", help="use onnxsim or not")
+    # parser.add_argument("--no-onnxsim", action="store_true", help="use onnxsim or not")
     parser.add_argument(
         "-f",
         "--exp_file",
