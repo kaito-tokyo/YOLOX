fec0a513088e72305ec3c040b61f2f638af7aef6
Kaito Udagawa <umireon@gmail.com>
2025-09-21 15:14:54 +0900

    Update export_coreml.py

--- tools/export_coreml.py
+++ tools/export_coreml.py
@@ -8,6 +8,7 @@ from loguru import logger
 
 import torch
 from torch import nn
+import torchvision
 
 import coremltools as ct
 
@@ -39,6 +40,56 @@ def make_parser():
     )
     return parser
 
+class YOLOX_with_postprocess(nn.Module):
+    def __init__(self, model, conf_thre=0.25, nms_thre=0.45, class_agnostic=False):
+        super().__init__()
+        self.model = model
+
+        self.conf_thre = conf_thre
+        self.nms_thre = nms_thre
+
+        self.class_agnostic = class_agnostic
+
+    def forward(self, x):
+        prediction = self.model(x).squeeze()
+        box_corner = prediction.new(prediction.shape)
+        box_corner[:, 0] = prediction[:, 0] - prediction[:, 2] / 2
+        box_corner[:, 1] = prediction[:, 1] - prediction[:, 3] / 2
+        box_corner[:, 2] = prediction[:, 0] + prediction[:, 2] / 2
+        box_corner[:, 3] = prediction[:, 1] + prediction[:, 3] / 2
+        prediction[:, :4] = box_corner[:, :4]
+
+        class_conf, class_pred = torch.max(prediction[:, 5:], 1)
+
+        conf_mask = prediction[:, 4] * class_conf >= self.conf_thre
+
+        (rows,) = conf_mask.nonzero(as_tuple=True)
+
+        confident_candidates = prediction[rows]
+        confident_class_conf = class_conf[rows]
+        confident_class_pred = class_pred[rows]
+        confident_conf = confident_candidates[:, 4] * confident_class_conf
+
+        if self.class_agnostic:
+            nms_out_index = torchvision.ops.nms(
+                confident_candidates[:, :4],
+                confident_conf,
+                self.nms_thre,
+            )
+        else:
+            nms_out_index = torchvision.ops.batched_nms(
+                confident_candidates[:, :4],
+                confident_conf,
+                confident_class_pred,
+                self.nms_thre,
+            )
+
+        final_detections = confident_candidates[nms_out_index]
+        final_class_pred = confident_class_pred[nms_out_index]
+        final_conf = confident_conf[nms_out_index]
+
+        return final_detections[:, :4], final_conf, final_class_pred
+
 
 @logger.catch
 def main():
@@ -63,23 +114,35 @@ def main():
         ckpt = ckpt["model"]
     model.load_state_dict(ckpt)
     model = replace_module(model, nn.SiLU, SiLU)
-    model.head.decode_in_inference = False
+    model.head.decode_in_inference = True
 
+    wrapped_model = YOLOX_with_postprocess(
+        model,
+        class_agnostic=True,
+    )
+    wrapped_model.eval()
 
     logger.info("loading checkpoint done.")
     dummy_input = torch.randn(1, 3, exp.test_size[0], exp.test_size[1])
 
-    traced_model = torch.jit.trace(model, dummy_input)
+    traced_model = torch.jit.trace(wrapped_model, dummy_input)
     
     image_input = ct.ImageType(
-        shape=dummy_input.shape,
+        name="image",
+        shape=(1, 3, exp.test_size[0], exp.test_size[1]),
         color_layout=ct.colorlayout.BGR
     )
 
     mlmodel = ct.convert(
         traced_model,
         inputs=[image_input],
+        outputs=[
+            ct.TensorType(name="coordinates"),
+            ct.TensorType(name="confidence"),
+            ct.TensorType(name="labels"),
+        ],
         convert_to="mlprogram",
+        compute_units=ct.ComputeUnit.ALL,
     )
     
     mlmodel.save(args.output_name)
